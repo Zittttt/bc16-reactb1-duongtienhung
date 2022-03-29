6a86b599c0137d4e47e0a92612ced6f5653ea591
Hưng Dương <rememberminato@gmail.com>
2022-03-29 23:46:21 +0700

    'btreactb1'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapThucHanhLayout from './Component/BaiTapLayoutComponent/BaiTapThucHanhLayout';
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapThucHanhLayout/>
+    </div>
+  );
+}
+
+export default App;
